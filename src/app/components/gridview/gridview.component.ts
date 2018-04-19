01788d74c7b7db339688981d843917a48640f0fc
MikeaMihali <mikea.mihali@fshnstudent.info>
2018-04-19 21:18:35 +0200

    fixed location

--- src/app/components/gridview/gridview.component.ts
+++ src/app/components/gridview/gridview.component.ts
@@ -11,9 +11,10 @@ import 'rxjs/add/operator/map';
   styleUrls: ['./gridview.component.css']
 })
 export class GridviewComponent implements OnInit {
-listofcam:any[];
-listofcity:any[]=[];
-selected:any;
+private response:any[];
+// private listofcam:any[];
+// private listofcity:any[];
+private selected:any;
 @ViewChild('nav', {read: DragScrollDirective}) ds: DragScrollDirective;
   
   moveLeft() {
@@ -25,43 +26,21 @@ selected:any;
   }
   constructor(private service:PlaystreamService,private updateservice:ClickmapService,private http:HttpClient) { 
       this.updateservice.getUpdatedList().subscribe(list=>{
-        this.listofcam=list;
-        let url="http://api.geonames.org/findNearbyPlaceNameJSON";
-        for(let cam of this.listofcam){
-          let Params = new HttpParams();
-          Params=Params.append('lat',cam.location.x);
-          Params=Params.append('lng',cam.location.y);
-          Params=Params.append('username','xhesina');
-          console.log(cam.location.x+" "+cam.location.y);
-          this.http.get(url,{
-              params:Params
-      
-          }).subscribe(response=>{
-            console.log(response);
-            this.listofcity.push(response);
-          });
-        }
-        console.log(this.listofcity);
+        this.response=list;
+       // console.log(this.response);
       });
+    
   }
   selectCam(i:number){
-    this.service.setSource("http://videos3.earthcam.com"+this.listofcam[i].specificProperties.source);
+    this.service.setSource("http://videos3.earthcam.com"+this.response[i].apirefat.specificProperties.source);
     this.selected={
-      'city':this.listofcity[i].geonames[0].name,
-      'country':this.listofcity[i].geonames[0].countryName,
-      'lat':this.listofcam[i].location.x,
-      'long':this.listofcam[i].location.y,
+      'city':this.response[i].api2.geonames[0].name,
+      'country':this.response[i].api2.geonames[0].countryName,
+      'lat':this.response[i].apirefat.location.x,
+      'long':this.response[i].apirefat.location.y,
 
     };
-    // this.isselect=true;
   }
-  // getCity(){
-   
-    
-  // }
-  // getCountry(x:string,y:string){
-  //   return "kjo eshte prove"+x+y;
-  // }
   ngOnInit() {
   }
 }
--- src/app/services/clickmap.service.ts
+++ src/app/services/clickmap.service.ts
@@ -7,8 +7,8 @@ import { icon,marker } from 'leaflet';
 export class ClickmapService {
   private subject=new Subject<any>();
   private listofcam:any;
-  private address:any[]=[];
   private markers:any[]=[];
+  private test:any[]=[];
   private url="http://street-asset-manager-api.herokuapp.com/v1/all/category=camera";
   constructor(private http:HttpClient) {
     this.http.get(this.url).subscribe(response=>{
@@ -27,20 +27,40 @@ export class ClickmapService {
    }
    updateList(){
     this.http.get(this.url).subscribe(response=>{
-      this.listofcam=response;
-      this.subject.next(this.listofcam);
+     this.listofcam=response;
+     let url="http://api.geonames.org/findNearbyPlaceNameJSON";
+     let temp:any;
+        for(let cam of this.listofcam){
+          let Params = new HttpParams();
+          Params=Params.append('lat',cam.location.x);
+          Params=Params.append('lng',cam.location.y);
+          Params=Params.append('username','xhesina');
+         // console.log(cam.location.x+" "+cam.location.y);
+          this.http.get(url,{
+              params:Params
+      
+          }).subscribe(response=>{
+              temp={
+                'apirefat': cam,
+                'api2':response
+              }
+              this.test.push(temp);
+          });
+        }
+      this.subject.next(this.test);
     });
+    this.test=[];
    }
-   testSearch(polygon:any){
-     let urltest="http://street-asset-manager-api.herokuapp.com/v1/all/search/geography";
-     this.http.post(urltest,{
-       params: new HttpParams().set('area',JSON.stringify(polygon)),
-       headers:{'Content-Type': 'application/json'}
-     },).subscribe(response=>{
-     // console.log(response);
-      console.log(JSON.stringify(polygon));
-    });
-   }
+  //  testSearch(polygon:any){
+  //    let urltest="http://street-asset-manager-api.herokuapp.com/v1/all/search/geography";
+  //    this.http.post(urltest,{
+  //      params: new HttpParams().set('area',JSON.stringify(polygon)),
+  //      headers:{'Content-Type': 'application/json'}
+  //    },).subscribe(response=>{
+  //    // console.log(response);
+  //     console.log(JSON.stringify(polygon));
+  //   });
+  //  }
    getUpdatedList():Observable<any>{
      return this.subject.asObservable();
    }
